c5dc3ee9bfb4515223e14e94c8e546aa4db14ab3
Thiago F Couve <31904362+TFCOUVE@users.noreply.github.com>
2019-09-09 21:42:44 -0300

    [HOT-FIX] - Giant hot-fix because everything i done is wrong so i do this shit rigth this time

--- ac2/Routes.py
+++ ac2/Routes.py
@@ -1,109 +1,167 @@
-from flask import request, jsonify
+from flask import request, jsonify, Response
 from main import App
 
 
 @App.route('/status')
 def status():
-    return 'Everything Ok. :)', 200
+    return Response('Everything Ok. :)', 200)
 
 
-# TODO NEED TO VERIFY IF THE VALUE HAS ALL THE REQUIREMENTS TO ENTER DATABASE LIKE _ IN TELEGRAM AND @ IN EMAIL
 @App.route('/create_user', methods=["POST"])
 def insert_user():
-    from ac2.Services.UserService import recovery_value
-    from ac2.Services.UserService import create_value
     data = request.get_json()
-    user = data["id"]
-    if user is None:
-        return 'Need User'
-    value = data["value"]
-    if value is None:
-        return 'Need Value'
-    value_type = data["type"]
-    if value_type is None:
-        return 'Need Type'
-    x = recovery_value(value, value_type)
-    if x is not None:
-        return 'Value already registered', 200
+    from ac2.Services.UserService import verify_json
+    boolean_json = verify_json(data)
+    if boolean_json is not True:
+        return Response('Json format incorrect')
     else:
-        try:
-            record_id = create_value(user, value, value_type)
-            from ac2.Utils.Config import generic_response
-            response = generic_response('Succeed', 'Value Creation succeed', record_id)
-            return jsonify(response), 200
-        except Exception as e:
-            print(e)
-            return e
+        from ac2.Services.UserService import verify_db_value
+        boolean_value = verify_db_value(data["value"], data["type"])
+        if boolean_value is not None:
+            return Response('Confirmed Value Already Recorded')
+        else:
+            try:
+                from ac2.Services.UserService import create_value
+                response = create_value(data["user_id"], data["value"], data["type"])
+                return jsonify(response), 200
+            except Exception as e:
+                print(e)
+                return Response(e, 500)
 
 
-@App.route('/list_values/<int:user_id>')
-def list_values(user_id):
-    from ac2.Services.UserService import list_many_values
-    try:
-        from ac2.Utils.Config import generic_response
-        data = list_many_values(user_id)
-        response = generic_response('Succeed', 'Found Value', data=data)
-        return jsonify(response), 200
-    except Exception as e:
-        print(e)
-        return e
-
-
-# TODO LEFT TO FINISH
-@App.route('/activate_value', methods=["PUT"])
-def activate_value():
-    from ac2.Services.UserService import activate_value
+@App.route('/confirm_value', methods=["POST"])
+def confirm_value():
     data = request.get_json()
-    user_id = data["id"]
-    if user_id is None:
-        return 'Need Id'
-    value = data["value"]
-    if value is None:
-        return 'Need Value'
-    value_type = data["type"]
-    if value_type is None:
-        return 'Need Type'
-    from ac2.Services.UserService import recovery_value
-    x = recovery_value(value, value_type)
-    if x is None:
-        return 'Value not registered', 200
-    try:
-        record_id = activate_value(value, value_type, user_id)
-        from ac2.Utils.Config import generic_response
-        response = generic_response('Succeed', 'Value activation succeed', record_id)
-        return jsonify(response), 200
-    except Exception as e:
-        print(e)
-        return e
+    from ac2.Services.UserService import verify_json
+    boolean_json = verify_json(data)
+    if boolean_json is not True:
+        return Response('Json format incorrect')
+    else:
+        from ac2.Services.UserService import verify_value
+        boolean_db_value = verify_value()
+        if boolean_db_value is False:
+            return Response('Value Incorrect', 406)
+        else:
+            from ac2.Services.UserService import verify_db_value
+            boolean_value = verify_db_value(data["value"], data["type"])
+            if boolean_value is not None:
+                from ac2.Services.UserService import activate_value
+                response = activate_value(boolean_db_value)
+                return jsonify(response), 200
+            else:
+                return Response('Value not registered', 406)
 
 
 @App.route('/cancel_value', methods=["POST"])
-def value_cancel():
-    from ac2.Services.UserService import activate_value
+def cancel_value():
     data = request.get_json()
-    user_id = data["id"]
-    if user_id is None:
-        return 'Need Id'
-    value = data["value"]
-    if value is None:
-        return 'Need Value'
-    value_type = data["type"]
-    if value_type is None:
-        return 'Need Type'
-    from ac2.Services.UserService import recovery_value
-    x = recovery_value(value, value_type)
-    if x is None:
-        return 'Value not registered', 200
-    try:
-        record_id = activate_value(value, value_type)
-        from ac2.Utils.Config import generic_response
-        response = generic_response('Succeed', 'Value activation succeed', record_id)
-        return jsonify(response), 200
-    except Exception as e:
-        print(e)
-        return e
+    from ac2.Services.UserService import verify_json
+    boolean_json = verify_json(data)
+    if boolean_json is not True:
+        return Response('Json format incorrect')
+    else:
+        from ac2.Services.UserService import verify_value
+        boolean_db_value = verify_value()
+        if boolean_db_value is False:
+            return Response('Value Incorrect', 406)
+        else:
+            from ac2.Services.UserService import verify_db_value
+            boolean_value = verify_db_value(data["value"], data["type"])
+            if boolean_value is not None:
+                from ac2.Services.UserService import canceled_value
+                response = canceled_value(boolean_db_value)
+                return jsonify(response), 200
+            else:
+                return Response('Value not registered', 406)
 
-
-@App.route('/list_by_value_type',  methods=["POST"])
-def list_value():
-    pass
+# from ac2.Services.UserService import recovery_value
+# x = recovery_value(value, value_type)
+# if x is not None:
+#     return 'Value already registered', 200
+# else:
+#     try:
+#         from ac2.Services.UserService import create_value
+#         record_id = create_value(user, value, value_type)
+#        from ac2.Utils.Config import generic_response
+#         response = generic_response('Succeed', 'Value Creation succeed', record_id)
+#         return jsonify(response), 200
+#     except Exception as e:
+#         print(e)
+#         return Response(e, 500)
+#
+#
+# @App.route('/list_values/<int:user_id>')
+# def list_values(user_id):
+#     from ac2.Services.UserService import list_many_values
+#     try:
+#         from ac2.Utils.Config import generic_response
+#         data = list_many_values(user_id)
+#         response = generic_response('Succeed', 'Found Value', data=data)
+#         return jsonify(response), 200
+#     except Exception as e:
+#         print(e)
+#         return e
+#
+#
+# @App.route('/activate_value', methods=["POST"])
+# def activate_value():
+#     from ac2.Services.UserService import activate_value
+#     data = request.get_json()
+#     user_id = data["id"]
+#     if user_id is None:
+#         return 'Need Id'
+#     value = data["value"]
+#     if value is None:
+#         return 'Need Value'
+#     value_type = data["type"]
+#     if value_type is None:
+#         return 'Need Type'
+#     from ac2.Services.UserService import recovery_value
+#     x = recovery_value(value, value_type)
+#     if x is None:
+#         return 'Value not registered', 200
+#     try:
+#         record_id = activate_value(value, value_type, user_id)
+#         from ac2.Utils.Config import generic_response
+#         response = generic_response('Succeed', 'Value activation succeed', record_id)
+#         return jsonify(response), 200
+#     except Exception as e:
+#         print(e)
+#         return e
+#
+#
+# @App.route('/cancel_value', methods=["POST"])
+# def value_cancel():
+#     from ac2.Services.UserService import activate_value
+#     data = request.get_json()
+#     user_id = data["id"]
+#     if user_id is None:
+#         return 'Need Id'
+#     value = data["value"]
+#     if value is None:
+#         return 'Need Value'
+#     value_type = data["type"]
+#     if value_type is None:
+#         return 'Need Type'
+#     from ac2.Services.UserService import recovery_value
+#     x = recovery_value(value, value_type)
+#     if x is None:
+#         return 'Value not registered', 200
+#     try:
+#         record_id = activate_value(value, value_type, user_id)
+#         from ac2.Utils.Config import generic_response
+#         response = generic_response('Succeed', 'Value activation succeed', record_id)
+#         return jsonify(response), 200
+#     except Exception as e:
+#         print(e)
+#         return e
+#
+#
+# @App.route('/list_by_value_type', methods=["POST"])
+# def list_value():
+#     pass
+#
+#
+# @App.route('/verify_value', methods=["POST"])
+# def create_user():
+#     pass
--- ac2/Services/UserService.py
+++ ac2/Services/UserService.py
@@ -1,88 +1,143 @@
-def create_value(user_id, value, value_type):
-    from ac2.Model.Record import Record
-    rec = Record()
-    rec.telephone_flag = False
-    rec.telegram_flag = False
-    rec.email_flag = False
-    if value_type == 'telegram':
-        rec.telegram_flag = True
-    elif value_type == 'telephone':
-        rec.telephone_flag = True
-    elif value_type == 'email':
-        rec.email_flag = True
-    else:
-        return 'Type Value Incorrect'
-    rec.value = value
-    rec.user_id = user_id
-    from main import db
-    db.session.add(rec)
-    db.session.commit()
-    return rec.id
+from sqlalchemy import and_
 
 
-def recovery_value(value, value_type):
+def verify_json(json):
+    user = json["user_id"]
+    if user is None:
+        return False
+    value = json["value"]
+    if value is None:
+        return False
+    value_type = json["type"]
+    if value_type is None:
+        return False
+    return True
+
+
+def verify_db_value(json_value, json_value_type):
     from ac2.Model.Record import Record
-    email = 0
-    telegram = 0
-    telephone = 0
-    if value_type == 'email':
-        email = 1
-    elif value_type == 'telegram':
-        telegram = 1
-    elif value_type == 'telephone':
-        telephone = 1
-    data = Record.query.filter_by(value=value, email_flag=email, telephone_flag=telephone,
-                                  telegram_flag=telegram).first()
+    data = Record.query.filter_by(value=json_value, value_type=json_value_type).first()
     if data is None:
         return None
     else:
         return data.id
 
 
-def list_many_values(user_id):
+def create_value(json_user_id, json_value, json_value_type):
     from ac2.Model.Record import Record
-    data = Record.query.filter_by(user_id=user_id)
-    records = []
-    if data is None:
-        return None
-    else:
-        for row in data:
-            dic = {"ID": row.id, "USER_ID": row.user_id, "VALUE": row.value}
-            if row.telephone_flag is True:
-                dic.update({"TYPE": 'Telephone'})
-            elif row.telegram_flag is True:
-                dic.update({"TYPE": 'Telegram'})
-            elif row.email_flag is True:
-                dic.update({"TYPE": 'Email'})
-            else:
-                dic.update({"TYPE": 'Not Recorded'})
-            records.append(dic)
-    return records
+    rec = Record()
+    rec.value_type = json_value_type.lower()
+    rec.value = json_value
+    rec.user_id = json_user_id
+    from main import db
+    db.session.add(rec)
+    db.session.commit()
+    return rec.id
 
 
-def activate_value(value, value_type, user_id):
+def verify_value():
+    return True
+
+
+def activate_value(record_id):
     from ac2.Model.Record import Record
     from main import db
-    record_id = recovery_value(value, value_type)
     rec = Record.query.filter_by(id=record_id).first()
-    if rec.user_id == user_id:
-        rec.id = record_id
-    else:
-        return 'User Id Incorrect'
+    rec_id = rec.id
+    rec_value = rec.value
     rec.confirmed_status = 'Confirmed'
     db.session.commit()
-    return 'Value Activated'
+    data = Record.query.filter(and_(Record.id.notilike(rec_id), Record.value.like(rec_value)))
+    for row in data:
+        row.confirmed_status = 'Canceled'
+        db.session.commit()
+    return 'Value Activated', 200
 
 
-def cancel_value(value, value_type, user_id):
+def canceled_value(record_id):
     from ac2.Model.Record import Record
     from main import db
-    record_id = recovery_value(value, value_type)
     rec = Record.query.filter_by(id=record_id).first()
-    if rec.user_id == user_id:
-        rec.id = record_id
-    else:
-        return 'User Id Incorrect'
-    rec.confirmed_status = 'Cancel'
+    rec.confirmed_status = 'Canceled'
     db.session.commit()
-    return 'Value Canceled'
+    return 'Value Cancel', 200
+
+
+# def create_value(user_id, value, value_type):
+#     from ac2.Model.Record import Record
+#     rec = Record()
+#     rec.telephone_flag = False
+#     rec.telegram_flag = False
+#     rec.email_flag = False
+#     if value_type == 'telegram':
+#         rec.telegram_flag = True
+#     elif value_type == 'telephone':
+#         rec.telephone_flag = True
+#     elif value_type == 'email':
+#         rec.email_flag = True
+#     else:
+#         return 'Type Value Incorrect'
+#     rec.value = value
+#     rec.user_id = user_id
+#     from main import db
+#     db.session.add(rec)
+#     db.session.commit()
+#     return rec.id
+#
+#
+# def recovery_value(value, value_type):
+#     from ac2.Model.Record import Record
+#     data = Record.query.filter_by(value=value, value_type=value_type, confirmed_status='Confirmed').first()
+#     if data is None:
+#         return None
+#     else:
+#         return data.id
+#
+#
+# def list_many_values(user_id):
+#     from ac2.Model.Record import Record
+#     data = Record.query.filter_by(user_id=user_id)
+#     records = []
+#     if data is None:
+#         return None
+#     else:
+#         for row in data:
+#             dic = {"ID": row.id, "USER_ID": row.user_id, "VALUE": row.value}
+#             if row.telephone_flag is True:
+#                 dic.update({"TYPE": 'Telephone'})
+#             elif row.telegram_flag is True:
+#                 dic.update({"TYPE": 'Telegram'})
+#             elif row.email_flag is True:
+#                 dic.update({"TYPE": 'Email'})
+#             else:
+#                 dic.update({"TYPE": 'Not Recorded'})
+#             records.append(dic)
+#     return records
+#
+#
+# def activate_value(value, value_type, user_id):
+#     from ac2.Model.Record import Record
+#     from main import db
+#     record_id = recovery_value(value, value_type)
+#     rec = Record.query.filter_by(id=record_id).first()
+#     if rec.user_id == user_id:
+#         rec.id = record_id
+#     else:
+#         return 'User/Type Incorrect', 409
+#     rec.confirmed_status = 'Confirmed'
+#     db.session.commit()
+#     return 'Value Activated'
+#
+#
+# def cancel_value(value, value_type, user_id):
+#     from ac2.Model.Record import Record
+#     from main import db
+#     record_id = recovery_value(value, value_type)
+#     rec = Record.query.filter_by(id=record_id).first()
+#     if rec.user_id == user_id:
+#         rec.id = record_id
+#     else:
+#         return 'User Id Incorrect'
+#     rec.confirmed_status = 'Cancel'
+#     db.session.commit()
+#     return 'Value Canceled'
